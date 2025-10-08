024bf2b1e8519f14082f233054f486bdca49c70f
prPedr <prpedro025@gmail.com>
2025-10-08 04:45:29 -0300

    Criacao da conecxao com o banco de dados postresql

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,15 +5,25 @@ import cors from "cors"
 
 import routes from "./routes/index.js"
 import ErrorHandleMiddleares from "@shared/middlewares/ErrorHandleMiddleares.js"
+import { AppDataSource } from "@shared/typeorm/dataSource.js"
 
-const app = express()
+AppDataSource.initialize()
+    .then(async () => {
+        const app = express()
 
-app.use(cors())
-app.use(express.json())
+        app.use(cors())
+        app.use(express.json())
 
-app.use(routes)
-app.use(ErrorHandleMiddleares.handleError)
+        app.use(routes)
+        app.use(ErrorHandleMiddleares.handleError)
 
-app.listen(3511, () => {
-    console.log("Servidor rodando na porta 3511")
-})
+        console.log("Conectado ao banco de dados")
+
+        app.listen(3511, () => {
+            console.log("Servidor rodando na porta 3511")
+        })
+    })
+
+    .catch((error) => {
+        console.error("Erro ao conectar com o banco de dados", error)
+    })
---
+++ src/shared/typeorm/dataSource.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import "dotenv/config";
+import { DataSource } from "typeorm";
+
+const portaDb = process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432;
+
+if (!process.env.DB_HOST || !process.env.DB_USERNAME || !process.env.DB_PASSWORD || !process.env.DB_DATABASE) {
+    throw new Error("Variáveis de ambiente do banco não estão definidas!");
+}
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: portaDb,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
